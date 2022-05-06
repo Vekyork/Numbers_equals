6094d94ae461a7271567735900e10f0a17bc2c91
Vekyork <vekyork@yandex.ru>
2022-05-06 20:57:48 +0300

    Сравнение чисел с try/catch

--- app/src/main/java/com/example/equals_numbers/MainActivity.java
+++ app/src/main/java/com/example/equals_numbers/MainActivity.java
@@ -22,17 +22,17 @@ public class MainActivity extends AppCompatActivity {
         final EditText editText_2 = findViewById(R.id.editText_2);
         final TextView textView = findViewById(R.id.textView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        button.setOnClickListener(v -> {
+            try {
                 arg_1 = Integer.valueOf(editText_1.getText().toString());
                 arg_2 = Integer.valueOf(editText_2.getText().toString());
-
-                if (arg_1.equals(arg_2)){
+                if (arg_1.equals(arg_2)) {
                     textView.setText("Равно!");
-                }else {
+                } else {
                     textView.setText("Не равно!");
                 }
+            }catch (NumberFormatException e){
+                e.printStackTrace ();
             }
         });
 
